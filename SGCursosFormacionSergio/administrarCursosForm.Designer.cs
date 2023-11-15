1b06e087ad911b5f841414812d85b60b2e5588a0
SergioAlmagre <127857356+SergioAlmagre@users.noreply.github.com>
2023-11-15 12:37:09 +0100

    Empezamos

--- SGCursosFormacionSergio/administrarCursosForm.Designer.cs
+++ SGCursosFormacionSergio/administrarCursosForm.Designer.cs
@@ -28,7 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.dsDB = new SGCursosFormacionSergio.dsDB();
+            this.fAMILIASBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.fAMILIASTableAdapter = new SGCursosFormacionSergio.dsDBTableAdapters.FAMILIASTableAdapter();
+            this.tableAdapterManager = new SGCursosFormacionSergio.dsDBTableAdapters.TableAdapterManager();
+            ((System.ComponentModel.ISupportInitialize)(this.dsDB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fAMILIASBindingSource)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dsDB
+            // 
+            this.dsDB.DataSetName = "dsDB";
+            this.dsDB.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // fAMILIASBindingSource
+            // 
+            this.fAMILIASBindingSource.DataMember = "FAMILIAS";
+            this.fAMILIASBindingSource.DataSource = this.dsDB;
+            // 
+            // fAMILIASTableAdapter
+            // 
+            this.fAMILIASTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.ALUMNOSTableAdapter = null;
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.CURSOSTableAdapter = null;
+            this.tableAdapterManager.ESTADOSTableAdapter = null;
+            this.tableAdapterManager.FAMILIASTableAdapter = this.fAMILIASTableAdapter;
+            this.tableAdapterManager.UpdateOrder = SGCursosFormacionSergio.dsDBTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
             // 
             // administrarCursosForm
             // 
@@ -38,10 +68,18 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "administrarCursosForm";
             this.Text = "administrarCursosForm";
+            this.Load += new System.EventHandler(this.administrarCursosForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dsDB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fAMILIASBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private dsDB dsDB;
+        private System.Windows.Forms.BindingSource fAMILIASBindingSource;
+        private dsDBTableAdapters.FAMILIASTableAdapter fAMILIASTableAdapter;
+        private dsDBTableAdapters.TableAdapterManager tableAdapterManager;
     }
 }
--- SGCursosFormacionSergio/administrarCursosForm.cs
+++ SGCursosFormacionSergio/administrarCursosForm.cs
@@ -16,5 +16,13 @@ namespace SGCursosFormacionSergio
         {
             InitializeComponent();
         }
+
+
+        private void administrarCursosForm_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'dsDB.FAMILIAS' Puede moverla o quitarla según sea necesario.
+            this.fAMILIASTableAdapter.Fill(this.dsDB.FAMILIAS);
+
+        }
     }
 }
